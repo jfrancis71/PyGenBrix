4e155d21c0b53b2e94e1a7e0041e8b395e3ad1c9
Julian <julian.w.francis@gmail.com>
2020-05-02 13:32:01 +0100

    Making pixel distribution configurable

--- VariationalAutoencoder.py
+++ VariationalAutoencoder.py
@@ -3,8 +3,13 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
+class BernoulliConditionalDistribution():
+    def log_prob( self, samples, conditionals ):
+        log_probs = torch.distributions.Bernoulli( logits = conditionals ).log_prob( samples )
+        return log_probs
+
 class VAE(nn.Module):
-    def __init__( self, device ):
+    def __init__( self, p_conditional_distribution, device ):
         super(VAE, self).__init__()
 
         self.fc1 = nn.Linear(784, 400).to( device )
@@ -13,6 +18,7 @@ class VAE(nn.Module):
         self.fc3 = nn.Linear(20, 400).to( device )
         self.fc4 = nn.Linear(400, 784).to( device )
 
+        self.p_conditional_distribution = p_conditional_distribution
         self.device = device
 
     def encode(self, x):
@@ -33,10 +39,11 @@ class VAE(nn.Module):
         mu, logvar = self.encode(x.view(-1, 784))
         z = self.sample_z(mu, logvar)
         decode_params = self.decode( z )
-        recons_log_prob = torch.sum( torch.distributions.Bernoulli( logits = decode_params ).log_prob( x ) )
+        recons_log_prob = self.p_conditional_distribution.log_prob( x, decode_params )
+        recons_log_prob_sum = torch.sum( recons_log_prob )
         kl_divergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         
-        return recons_log_prob - kl_divergence
+        return recons_log_prob_sum - kl_divergence
 
     def sample( self ):
         recon_x = self.decode( torch.tensor( np.random.normal( size = [ 1, 20 ] ).astype( np.float32 ) ).to( self.device ) )
