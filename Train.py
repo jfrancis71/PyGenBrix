be214f59d876a2e34727e0300d7ecb8e41814b0d
Julian <julian.w.francis@gmail.com>
2020-05-05 13:48:03 +0100

    Adding validation

--- Train.py
+++ Train.py
@@ -1,6 +1,7 @@
 import torch.optim as optim
 import torch
 import time
+import numpy as np
 
 def partition(l, n):
     n = max(1, n)
@@ -9,19 +10,35 @@ def partition(l, n):
 def train( model, samples, device = "CPU", epochs = 5000, batch_size = 32, sleep_time = 0 ):
     
     optimizer = optim.Adam( model.parameters(), lr=.001)
+    randomized_samples = np.random.permutation( samples )
+    training_size = np.round(randomized_samples.shape[0]*0.9).astype(int)
+    training_set = randomized_samples[:training_size]
+    validation_set = randomized_samples[training_size:min(training_size+64,randomized_samples.shape[0]-1)]
+
     for epoch in range(epochs):
-        running_loss = 0.0
-        batch_no = 0
-        for batch in partition( samples, batch_size ):
+        training_running_loss = 0.0
+        validation_running_loss = 0.0
+        training_batch_no = 0
+        validation_batch_no = 0
+        for batch in partition( training_set, batch_size ):
             tens = torch.tensor( batch ).to( device )
             dat = tens
             optimizer.zero_grad()
             result = model.log_prob( dat )
             loss = -result
-            running_loss += loss.item()
-            batch_no += 1
+            training_running_loss += loss.item()
+            training_batch_no += 1
             loss.backward()
             optimizer.step()
             time.sleep( sleep_time )
-        print( "Epoch ", epoch, ", Loss=", running_loss/batch_no )
+        for batch in partition( validation_set, batch_size ):
+            tens = torch.tensor( batch ).to( device )
+            dat = tens
+            optimizer.zero_grad()
+            result = model.log_prob( dat )
+            loss = -result
+            validation_running_loss += loss.item()
+            validation_batch_no += 1
+
+        print( "Epoch ", epoch, ", Training Loss=", training_running_loss/training_batch_no, ", Validation Loss ", validation_running_loss/validation_batch_no )
 
