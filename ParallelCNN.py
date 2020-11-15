3ddfface9c64e3d3f4641848baa69b766bdbc4cc
Julian <julian.w.francis@gmail.com>
2020-11-15 08:04:11 +0000

    Generalising MultiStageParallelCNN to support arbitrary number of levels

--- ParallelCNN.py
+++ ParallelCNN.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch
 import numpy as np
+import torch.nn.functional as F
 
 
 def generate_pixel_groups( height, width ):#1 means predict on this iteration
@@ -152,22 +153,23 @@ class Upsampler( nn.Module ):
         return samples
 
 class MultiStageParallelCNNDistribution( nn.Module ):
-    def __init__( self, dims, p_conditional_distribution, parallel_cnn_fn = default_parallel_cnn_fn ):
+#levels is number of upsampling levels, eg. 4 takes you from a 4x4 to 64x64. It has a minimum value of 1
+    def __init__( self, dims, p_conditional_distribution, levels, parallel_cnn_fn = default_parallel_cnn_fn ):
         super(MultiStageParallelCNNDistribution, self).__init__()
         self.p_conditional_distribution = p_conditional_distribution
-        self.parallelcnns = nn.ModuleList( create_parallelcnns( [ dims[0], dims[1]//8, dims[2]//8 ], p_conditional_distribution.params_size( dims[0] ), parallel_cnn_fn ) )
-        self.pixel_channel_groups = nn.Parameter( torch.tensor( generate_pixel_channel_groups( [ dims[0], dims[1]//8, dims[2]//8 ] ).astype( np.float32 ) ), requires_grad = False )
-        self.information_masks = nn.Parameter( torch.tensor( generate_information_masks( [ dims[0], dims[1]//8, dims[2]//8 ] ).astype( np.float32 ) ), requires_grad = False )
-
-        self.upsampler1 = Upsampler(  [ dims[0], dims[1]//16, dims[2]//16 ], p_conditional_distribution, parallel_cnn_fn )#Largest scale
-        self.upsampler2 = Upsampler(  [ dims[0], dims[1]//8, dims[2]//8 ], p_conditional_distribution, parallel_cnn_fn )#Large scale
-        self.upsampler3 = Upsampler(  [ dims[0], dims[1]//4, dims[2]//4 ], p_conditional_distribution, parallel_cnn_fn )#Fine scale
-        self.upsampler4 = Upsampler(  [ dims[0], dims[1]//2, dims[2]//2 ], p_conditional_distribution, parallel_cnn_fn )#Finest scale
+        #Bottom levels dims actually works on double resolution of first set of independent pixels.
+        #This is a bit of an implementation artefact of us reusing parallelcnn's for generating first level
+        self.bottom_level_dims = 2*dims[1]//(2**levels)
+        self.parallelcnns = nn.ModuleList( create_parallelcnns( [ dims[0], self.bottom_level_dims, self.bottom_level_dims ], p_conditional_distribution.params_size( dims[0] ), parallel_cnn_fn ) )
+        self.pixel_channel_groups = nn.Parameter( torch.tensor( generate_pixel_channel_groups( [ dims[0], self.bottom_level_dims, self.bottom_level_dims ] ).astype( np.float32 ) ), requires_grad = False )
+        self.information_masks = nn.Parameter( torch.tensor( generate_information_masks( [ dims[0], self.bottom_level_dims, self.bottom_level_dims ] ).astype( np.float32 ) ), requires_grad = False )
+        self.upsamplers = nn.ModuleList( [ Upsampler(  [ dims[0], dims[1]//(2**(levels-level)), dims[1]//(2**(levels-level)) ], p_conditional_distribution, parallel_cnn_fn ) for level in range( levels ) ] )
         self.dims = dims
+        self.levels = levels
 
     def log_prob( self, samples, conditional_inputs ):
-        subsamples = samples[:,:,::8,::8]
-        subconditional_inputs = conditional_inputs[:,:,::8,::8]
+        subsamples = samples[:,:,::2**(self.levels-1),::2**(self.levels-1)]
+        subconditional_inputs = conditional_inputs[:,:,::2**(self.levels-1),::2**(self.levels-1)]
         
         output_log_prob = torch.tensor( np.zeros( subsamples.shape[0] ) ).to( subsamples.device )
         for n in range( self.dims[0] ):
@@ -179,16 +181,15 @@ class MultiStageParallelCNNDistribution( nn.Module ):
             toutput_log_prob = self.p_conditional_distribution.log_prob( subsamples, subnet_output_logits, mask = self.pixel_channel_groups[n] )
             output_log_prob += toutput_log_prob
         
-        upsampled_log_prob1 = self.upsampler1.log_prob( samples[:,:,::8,::8], conditional_inputs[:,:,::8,::8] )
-        upsampled_log_prob2 = self.upsampler2.log_prob( samples[:,:,::4,::4], conditional_inputs[:,:,::4,::4] )
-        upsampled_log_prob3 = self.upsampler3.log_prob( samples[:,:,::2,::2], conditional_inputs[:,:,::2,::2] )
-        upsampled_log_prob4 = self.upsampler4.log_prob( samples, conditional_inputs )
-        
-        return output_log_prob + upsampled_log_prob1 + upsampled_log_prob2 + upsampled_log_prob3 + upsampled_log_prob4
+        upsampled_log_prob = output_log_prob
+        for level in range( self.levels ):
+            upsampled_log_prob += self.upsamplers[ level ].log_prob( samples[:,:,::2**(self.levels-level-1),::2**(self.levels-level-1)], conditional_inputs[:,:,::2**(self.levels-level-1),::2**(self.levels-level-1)] )
+
+        return upsampled_log_prob
 
     def sample( self, conditional_inputs ):
-        subsamples = torch.tensor( np.zeros( [ conditional_inputs.shape[0], self.dims[0], self.dims[1]//8, self.dims[2]//8 ] ).astype( np.float32 ) ).to( conditional_inputs.device )
-        subconditional_inputs = conditional_inputs[:,:,::8,::8]
+        subsamples = torch.tensor( np.zeros( [ conditional_inputs.shape[0], self.dims[0], self.bottom_level_dims, self.bottom_level_dims ] ).astype( np.float32 ) ).to( conditional_inputs.device )
+        subconditional_inputs = conditional_inputs[:,:,::2**(self.levels-1),::2**(self.levels-1)]
         
         for n in range( self.dims[0] ):
             subnet_input = torch.cat(
@@ -199,10 +200,63 @@ class MultiStageParallelCNNDistribution( nn.Module ):
             subsamples += self.p_conditional_distribution.sample( subnet_output_logits ) * \
                 self.pixel_channel_groups[n]
 
+        u = self.upsamplers[0].sample( subsamples[:,:,::2,::2], conditional_inputs[:,:,::2**(self.levels-1),::2**(self.levels-1)] )
+        for level in range( 1, self.levels ):
+            u = self.upsamplers[ level ].sample( u, conditional_inputs[:,:,::2**(self.levels-level-1),::2**(self.levels-level-1)] )
 
-        u1 = self.upsampler1.sample( subsamples[:,:,::2,::2], subconditional_inputs )
-        u2 = self.upsampler2.sample( u1, conditional_inputs[:,:,::4,::4] )
-        u3 = self.upsampler3.sample( u2, conditional_inputs[:,:,::2,::2] )
-        u4 = self.upsampler4.sample( u3, conditional_inputs )
+        return u
 
-        return u4
+activation_fn = F.relu
+
+#Achieved epoch 82, training 3620, validation 3902 on celeba aligned 20,000 images, batch size 16, level = 4, quantized output distribution
+class unet( nn.Module ):
+    def __init__( self, dims, params_size ):
+        super(unet, self).__init__()
+        self.upconv1_1 = torch.nn.Conv2d( dims[0]+1,64,3, padding=1 )
+        self.upconv1_2 = torch.nn.Conv2d( 64, 64, 3, padding=1)
+        
+        self.upconv2_1 = torch.nn.Conv2d( 64, 128, 3, padding = 1, stride = 2 )
+        self.upconv2_2 = torch.nn.Conv2d( 128, 128, 3, padding=1)
+        
+        self.downconv1_3 = torch.nn.Conv2d( 128, 64, 3, padding = 1 )
+        self.downconv1_2 = torch.nn.Conv2d( 64, 64, 3, padding = 1 )
+        self.downconv1_1 = torch.nn.Conv2d( 64, params_size, 1, padding = 0 )
+        
+        self.downconv2_2 = torch.nn.Conv2d( 128, 128, 3, padding = 1 )
+        self.downconv2_1 = torch.nn.Conv2d( 128, 128, 3, padding = 1 )
+        self.downconv2_upsample = torch.nn.Conv2d( 128, 256, 1, padding = 0 )
+
+    
+    def forward( self, x ):
+        x = self.upconv1_1( x )
+        x = activation_fn( x )
+        x = self.upconv1_2( x )
+        c1 = activation_fn( x )
+        
+        x = self.upconv2_1( c1 )
+        x = activation_fn( x )
+        x = self.upconv2_2( x )
+        c2 = activation_fn( x )
+        
+        x = self.downconv2_2( c2 )
+        x = activation_fn( x )
+        x = self.downconv2_1( x )
+        x = activation_fn( x )
+        x = self.downconv2_upsample( x )
+
+        orig = x.shape
+        x = x.permute(0, 2, 3, 1).view( orig[0], orig[2], orig[3], orig[1]//4, 2, 2)
+        x = x.permute(0, 1, 4, 2, 5, 3 ).contiguous().view( orig[0], orig[2]*2, orig[3]*2, orig[1]//4 )
+        x = x.permute( 0, 3, 1, 2 )
+        
+        x = torch.cat( [ c1, x ], axis = 1 )
+        x = self.downconv1_3( x )
+        x = activation_fn( x )
+        x = self.downconv1_2( x )
+        x = activation_fn( x )
+        x = self.downconv1_1( x )
+        
+        return x
+
+def unet_parallel_cnn_fn( dims, params_size ):
+    return unet( dims, params_size )
