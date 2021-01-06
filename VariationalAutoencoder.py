ae2c4f1b31b38623b15193688c50dffbe942d958
Julian <julian.w.francis@gmail.com>
2021-01-06 08:57:26 +0000

    Style improvements

--- VariationalAutoencoder.py
+++ VariationalAutoencoder.py
@@ -7,7 +7,7 @@ from PyGenBrix import VAEModels as vae_models
 
 class IndependentNormalDistribution():
     def __init__( self, loc, scale ):
-        self.dist = torch.distributions.Independent( torch.distributions.Normal( loc = loc, scale = scale ), 3 )
+        self.dist = torch.distributions.Independent( torch.distributions.Normal( loc = loc, scale = scale ), reinterpreted_batch_ndims = 3 )
     def log_prob( self, samples ):
         return { "log_prob" : self.dist.log_prob( samples ) }
 
@@ -16,89 +16,86 @@ class IndependentNormalDistribution():
 
 class IndependentL2Distribution():
     def __init__( self, loc ):
-        self.loc = torch.tanh( loc )
-    def log_prob( self, samples ): #.918... is log (1/sqrt(2 pi) )
-        return { "log_prob" : torch.sum( -0.918939 - 0.5 * (samples - self.loc)**2, dim = ( 1, 2, 3 ) ) }
+        self.dist = torch.distributions.Independent( torch.distributions.Normal( loc = loc, scale = torch.ones( loc.shape ) ), reinterpreted_batch_ndims = 3 )
+
+    def log_prob( self, samples ):
+        return { "log_prob" : self.dist.log_prob( samples ) }
 
     def sample( self ):
+#Note, not really sampling from prob distribution, but this is common in VAE literature,
+#where they return the mean as opposed to sampling from the normal distribution with variance 1.
         return self.loc
 
-
-    
 class IndependentBernoulliDistribution():
     def __init__( self, logits ):
-        self.dist = torch.distributions.Independent( torch.distributions.Bernoulli( logits = logits ), 3 )
+        self.dist = torch.distributions.Independent( torch.distributions.Bernoulli( logits = logits ), reinterpreted_batch_ndims = 3 )
     def log_prob( self, samples ):
         return { "log_prob" : self.dist.log_prob( samples ) }
 
     def sample( self ):
         return self.dist.sample()
 
+#Quantizes real number in interval [0,1] into 10 buckets
+class QuantizedDistribution():
+    def __init__( self, logits ):
+        reshaped_logits = torch.reshape( logits, ( logits.shape[0], logits.shape[1]//10, 10, logits.shape[2], logits.shape[3] ) )
+        reshaped_logits = reshaped_logits.permute( ( 0, 1, 3, 4, 2 ) )
+        self.dist = torch.distributions.Categorical( logits = reshaped_logits )
+
+    def log_prob( self, samples ):
+        quantized_samples = torch.clamp( (samples*10.0).floor(), 0, 9 )
+        log_prob = self.dist.log_prob( quantized_samples )
+        log_prob_sum = torch.sum( log_prob, dim = ( 1, 2, 3 ) )
+        return { "log_prob" : log_prob_sum }
+
+    def sample( self ):
+        return self.dist.sample()/10.0 + .05
 
 class IndependentL2Layer( nn.Module ):
 
-    def forward( self, logits ):
-        return IndependentL2Distribution( logits = logits )
+    def forward( self, distribution_params ):
+        return IndependentL2Distribution( loc = distribution_params )
 
     def params_size( self, channels ):
         return 1*channels
 
 class IndependentBernoulliLayer( nn.Module ):
 
-    def forward( self, logits ):
-        return IndependentBernoulliDistribution( logits = logits )
+    def forward( self, distribution_params ):
+        return IndependentBernoulliDistribution( logits = distribution_params )
 
     def params_size( self, channels ):
         return 1*channels
 
 class IndependentNormalLayer( nn.Module ):
 
-    def forward( self, logits ):
-        if ( logits.shape[1] % 2 != 0 ):
+    def forward( self, distribution_params ):
+        if ( distribution_params.shape[1] % 2 != 0 ):
             raise TypeError("channel size of logits must be an even number to encode means and scale, but it is of size {}"
-                            .format( logits.shape[1] ) )
-        output_channels = logits.shape[1] // 2
-        loc = logits[:,:output_channels]
-        scale = .05 + torch.nn.Softplus()( logits[:,output_channels:] )
+                            .format( distribution_params.shape[1] ) )
+        output_channels = distribution_params.shape[1] // 2
+        loc = distribution_params[:,:output_channels]
+        scale = .05 + torch.nn.Softplus()( distribution_params[:,output_channels:] )
         return IndependentNormalDistribution( loc = loc, scale = scale )
 
     def params_size( self, channels ):
         return 2*channels
 
-class QuantizedDistribution():
-    def __init__( self, logits ):
-        self.logits = logits
-
-    def log_prob( self, samples ):
-        quantized = torch.clamp( (samples*10.0).floor(), 0, 9 )
-        reshaped_conditionals = torch.reshape( self.logits, ( self.logits.shape[0], self.logits.shape[1]//10, 10, self.logits.shape[2], self.logits.shape[3] ) )
-        reshaped_conditionals = reshaped_conditionals.permute( ( 0, 1, 3, 4, 2 ) )
-        log_probs = torch.distributions.Categorical( logits = reshaped_conditionals ).log_prob( quantized )
-        log_probs_sum = torch.sum( log_probs, dim = ( 1, 2, 3 ) )
-        return { "log_prob" : log_probs_sum }
-
-    def sample( self ):
-        reshaped_conditionals = torch.reshape( self.logits, ( self.logits.shape[0], self.logits.shape[1]//10, 10, self.logits.shape[2], self.logits.shape[3] ) )
-        reshaped_conditionals = reshaped_conditionals.permute( ( 0, 1, 3, 4, 2 ) )
-        return torch.distributions.Categorical( logits = reshaped_conditionals ).sample()/10.0 + .05
-
-
 class QuantizedLayer( nn.Module ):
 
-    def forward( self, logits ):
-        return QuantizedDistribution( logits )
+    def forward( self, distribution_params ):
+        return QuantizedDistribution( logits = distribution_params )
 
     def params_size( self, channels ):
         return 10*channels
 
 
-
 class VAE(nn.Module):
     """
     to build:
-    mymodel = vae.VAE( vae_models.YZVAEModel(), vae.BernoulliConditionalDistribution() )
+    mymodel = vae.VAE( vae_models.MNISTVAEModel(), vae.IndependentBernoulliLayer() )
     or
-    mymodel = vae.VAE( vae_models.YZVAEModel(), cnn.ParallelCNNConditionalDistribution( [ 1, 28, 28 ], vae.QuantizedConditionalDistribution() ) )
+    mymodel = vae.VAE( vae_models.MNISTVAEModel(), cnn.MultiStageParallelCNNLayer( [ 1, 28, 28 ], vae.QuantizedLayer() ) )
     to train:
     Train.train( mydist, mnist, device, batch_size = 32 )
     """
@@ -123,10 +120,11 @@ class VAE(nn.Module):
         z = torch.distributions.normal.Normal( mu, torch.exp( 0.5*logvar ) ).rsample()
         decode_params = self.decode( z )
         recons_log_prob_dict = self.output_distribution_layer( decode_params ).log_prob( cx )
+        device = cx.device
         kl_divergence = torch.sum(
             torch.distributions.kl.kl_divergence(
                 torch.distributions.Normal( loc = mu, scale = torch.exp( 0.5*logvar ) ),
-	        torch.distributions.Normal( loc = torch.tensor( 0.0 ).to( next(self.decoder.parameters()).device ), scale = torch.tensor( 1.0 ).to( next(self.decoder.parameters()).device ) ) ),
+	        torch.distributions.Normal( loc = torch.tensor( 0.0 ).to( device ), scale = torch.tensor( 1.0 ).to( device ) ) ),
             dim = ( 1 ) )
         total_log_prob = recons_log_prob_dict["log_prob"] - kl_divergence
 
@@ -135,9 +133,10 @@ class VAE(nn.Module):
         return result_dict
 
     def sample( self, z = None ):
+        device = next(self.decoder.parameters()).device
         if z is not None:
             sample_z = z
         else:
             sample_z = np.random.normal( size = [ 1, self.vae_model.latents ] ).astype( np.float32 )
-        decode_params = self.decode( torch.tensor( sample_z ).to( next(self.decoder.parameters()).device ) )
+        decode_params = self.decode( torch.tensor( sample_z ).to( device ) )
         return self.output_distribution_layer( decode_params ).sample()
