585ea768b71f918277a06cff457a72c80dfb2926
Julian <julian.w.francis@gmail.com>
2021-01-07 15:27:40 +0000

    Stylistic improvements

--- VariationalAutoencoder.py
+++ VariationalAutoencoder.py
@@ -60,25 +60,25 @@ class MNISTVAE( BaseVAE ):
         self.dims = [ 1, 28, 28 ]
         self.encoder = \
             torch.nn.Sequential(
-                torch.nn.Conv2d( 1, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( base_depth, base_depth, 5, stride=2, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( base_depth, 2 * base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( 2*base_depth, 2 * base_depth, 5, stride=2, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( 2*base_depth, 4 * self.latents, 7, stride=1, padding=0 ), torch.nn.LeakyReLU(),
-                torch.nn.Flatten(),
-                torch.nn.Linear( 4 * self.latents, self.latents * 2 )
+                nn.Conv2d( 1, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( base_depth, base_depth, 5, stride=2, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( base_depth, 2 * base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( 2*base_depth, 2 * base_depth, 5, stride=2, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( 2*base_depth, 4 * self.latents, 7, stride=1, padding=0 ), torch.nn.LeakyReLU(),
+                nn.Flatten(),
+                nn.Linear( 4 * self.latents, self.latents * 2 )
         )
 
         self.decoder = \
             torch.nn.Sequential(
                 #note below layer turns into 7x7x....
-                torch.nn.ConvTranspose2d( self.latents, 2 * base_depth, 7, stride=1, padding= 0 ), torch.nn.LeakyReLU(),
-                torch.nn.ConvTranspose2d( 2 * base_depth, 2 * base_depth, 5, stride=1, padding= 2 ), torch.nn.LeakyReLU(),
-                torch.nn.ConvTranspose2d( 2 * base_depth, 2 * base_depth, 6, stride=2, padding=2 ), torch.nn.LeakyReLU(), #check padding 6
-                torch.nn.ConvTranspose2d( 2 * base_depth, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.ConvTranspose2d( base_depth, base_depth, 6, stride=2, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.ConvTranspose2d( base_depth, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( base_depth, output_distribution_layer.params_size( 1 ), kernel_size=5, stride=1, padding=2 )
+                nn.ConvTranspose2d( self.latents, 2 * base_depth, 7, stride=1, padding= 0 ), torch.nn.LeakyReLU(),
+                nn.ConvTranspose2d( 2 * base_depth, 2 * base_depth, 5, stride=1, padding= 2 ), torch.nn.LeakyReLU(),
+                nn.ConvTranspose2d( 2 * base_depth, 2 * base_depth, 6, stride=2, padding=2 ), torch.nn.LeakyReLU(), #check padding 6
+                nn.ConvTranspose2d( 2 * base_depth, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
+                nn.ConvTranspose2d( base_depth, base_depth, 6, stride=2, padding=2 ), torch.nn.LeakyReLU(),
+                nn.ConvTranspose2d( base_depth, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( base_depth, output_distribution_layer.params_size( 1 ), kernel_size=5, stride=1, padding=2 )
         )
 
 
@@ -91,25 +91,25 @@ class SmallRGBVAE( BaseVAE ):
 
         self.encoder = \
             torch.nn.Sequential(
-                torch.nn.Conv2d( 3, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( base_depth, base_depth, 5, stride=2, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( base_depth, 2 * base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( 2*base_depth, 2 * base_depth, 5, stride=2, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( 2*base_depth, 4 * self.latents, 8, stride=1, padding=0 ), torch.nn.LeakyReLU(),
-                torch.nn.Flatten(),
-                torch.nn.Linear( 4 * self.latents, self.latents * 2 )
+                nn.Conv2d( 3, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( base_depth, base_depth, 5, stride=2, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( base_depth, 2 * base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( 2*base_depth, 2 * base_depth, 5, stride=2, padding=2 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( 2*base_depth, 4 * self.latents, 8, stride=1, padding=0 ), torch.nn.LeakyReLU(),
+                nn.Flatten(),
+                nn.Linear( 4 * self.latents, self.latents * 2 )
             )
 
         self.decoder = \
             torch.nn.Sequential(
                 #note below layer turns into 8x8x....
-                torch.nn.ConvTranspose2d( self.latents, 2 * base_depth, 7, stride=1, padding= 0 ), torch.nn.LeakyReLU(),
-                torch.nn.ConvTranspose2d( 2 * base_depth, 2 * base_depth, 5, stride=1, padding= 2 ), torch.nn.LeakyReLU(),
-                torch.nn.ConvTranspose2d( 2 * base_depth, 2 * base_depth, 6, stride=2, padding=2 ), torch.nn.LeakyReLU(), #check padding 6
-                torch.nn.ConvTranspose2d( 2 * base_depth, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.ConvTranspose2d( base_depth, base_depth, 6, stride=2, padding=2 ), torch.nn.LeakyReLU(),
-                torch.nn.ConvTranspose2d( base_depth, base_depth, 5, stride=1, padding=0 ), torch.nn.LeakyReLU(),
-                torch.nn.Conv2d( base_depth, output_distribution_layer.params_size( 3 ), kernel_size=5, stride=1, padding=2 )
+                nn.ConvTranspose2d( self.latents, 2 * base_depth, 7, stride=1, padding= 0 ), torch.nn.LeakyReLU(),
+                nn.ConvTranspose2d( 2 * base_depth, 2 * base_depth, 5, stride=1, padding= 2 ), torch.nn.LeakyReLU(),
+                nn.ConvTranspose2d( 2 * base_depth, 2 * base_depth, 6, stride=2, padding=2 ), torch.nn.LeakyReLU(), #check padding 6
+                nn.ConvTranspose2d( 2 * base_depth, base_depth, 5, stride=1, padding=2 ), torch.nn.LeakyReLU(),
+                nn.ConvTranspose2d( base_depth, base_depth, 6, stride=2, padding=2 ), torch.nn.LeakyReLU(),
+                nn.ConvTranspose2d( base_depth, base_depth, 5, stride=1, padding=0 ), torch.nn.LeakyReLU(),
+                nn.Conv2d( base_depth, output_distribution_layer.params_size( 3 ), kernel_size=5, stride=1, padding=2 )
             )
 
 # 64x64x3 shaped model
@@ -122,20 +122,20 @@ class YZVAE( BaseVAE ):
 
         self.encoder = \
             torch.nn.Sequential(
-                torch.nn.Conv2d( 3, 64, kernel_size=5, padding=2, stride=2 ), torch.nn.ReLU(),
-                torch.nn.Conv2d( 64, 128, kernel_size=5, padding=2,stride=2 ), torch.nn.ReLU(),
-                torch.nn.Conv2d( 128, 256, kernel_size=5, padding=2,stride=2 ), torch.nn.ReLU(),
-                torch.nn.Conv2d( 256, 512, kernel_size=5, padding=2,stride=2 ), torch.nn.ReLU(),
-                torch.nn.Flatten(),
-                torch.nn.Linear( 512*4*4, self.latents * 2 )
+                nn.Conv2d( 3, 64, kernel_size=5, padding=2, stride=2 ), torch.nn.ReLU(),
+                nn.Conv2d( 64, 128, kernel_size=5, padding=2,stride=2 ), torch.nn.ReLU(),
+                nn.Conv2d( 128, 256, kernel_size=5, padding=2,stride=2 ), torch.nn.ReLU(),
+                nn.Conv2d( 256, 512, kernel_size=5, padding=2,stride=2 ), torch.nn.ReLU(),
+                nn.Flatten(),
+                nn.Linear( 512*4*4, self.latents * 2 )
             )
 
         self.decoder = \
             torch.nn.Sequential(
-                torch.nn.ConvTranspose2d( 512, 256, 8 ), torch.nn.LeakyReLU(), #256x8x8
-                torch.nn.ConvTranspose2d( 256, 256, 5, stride=2, output_padding=1, padding= 2 ), torch.nn.LeakyReLU(), #256x16x16, h1
-                torch.nn.ConvTranspose2d( 256, 64*2, 5, stride=2, padding= 2, output_padding=1 ), torch.nn.LeakyReLU(), #128x32x32, h2
-                torch.nn.ConvTranspose2d( 64*2, 64//2, 5, stride=2, padding=2, output_padding = 1 ), torch.nn.LeakyReLU(), #32x64x64, h3
-                torch.nn.ConvTranspose2d( 64//2, output_distribution_layer.params_size( 3 ), 5, stride=1, padding=2, output_padding = 0 ), torch.nn.LeakyReLU(), #h4
-#               torch.nn.ConvTranspose2d( 64, output_distribution_layer.params_size( 3 ), 5, stride=1, padding=2 )
+                nn.ConvTranspose2d( 512, 256, 8 ), torch.nn.LeakyReLU(), #256x8x8
+                nn.ConvTranspose2d( 256, 256, 5, stride=2, output_padding=1, padding= 2 ), torch.nn.LeakyReLU(), #256x16x16, h1
+                nn.ConvTranspose2d( 256, 64*2, 5, stride=2, padding= 2, output_padding=1 ), torch.nn.LeakyReLU(), #128x32x32, h2
+                nn.ConvTranspose2d( 64*2, 64//2, 5, stride=2, padding=2, output_padding = 1 ), torch.nn.LeakyReLU(), #32x64x64, h3
+                nn.ConvTranspose2d( 64//2, output_distribution_layer.params_size( 3 ), 5, stride=1, padding=2, output_padding = 0 ), torch.nn.LeakyReLU(), #h4
+#               nn.ConvTranspose2d( 64, output_distribution_layer.params_size( 3 ), 5, stride=1, padding=2 )
             )
