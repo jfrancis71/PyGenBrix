a3bc1c487b0eca2a73dd66ab3a244a13c0b9be0c
Julian <julian.w.francis@gmail.com>
2021-03-24 06:44:51 +0000

    sample() needs no_grad

--- PixelCNNDistribution.py
+++ PixelCNNDistribution.py
@@ -1,7 +1,7 @@
 #Uses https://github.com/jfrancis71/pixel-cnn-pp
 #Above was forked from Lucas Caccia repository, original: https://github.com/pclucas14/pixel-cnn-pp
 #See below, folder renamed from "pixel-cnn-pp" to "pixelcnn_pp" (for importing 
-into python)
+#into python)
 #This package should be searchable from python kernel.
 
 import torch
@@ -21,11 +21,12 @@ class PixelCNNDistribution(nn.Module):
         return {"log_prob": -pixelcnn_utils.discretized_mix_logistic_loss(samples, distribution_params)}
 
     def sample(self):
-        sampl = torch.zeros( (1,3,32,32), device="cuda")
-        for y in range(32):
-            for x in range(32):
-                distribution_params=self.pixelcnn_net((sampl*2)-1,sample=True)
-                s = pixelcnn_utils.sample_from_discretized_mix_logistic(distribution_params, 100)
-                sampl[0,:,y,x] = s[0,:,y,x]
+        with torch.no_grad():
+            sampl = torch.zeros( (1,3,32,32), device="cuda")
+            for y in range(32):
+                for x in range(32):
+                    distribution_params=self.pixelcnn_net((sampl*2)-1,sample=True)
+                    s = pixelcnn_utils.sample_from_discretized_mix_logistic(distribution_params, 100)
+                    sampl[0,:,y,x] = s[0,:,y,x]
         return (sampl+1.0)/2.0
         
