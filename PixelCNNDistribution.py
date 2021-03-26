cdd428bc0c2281b03a1bae7231071673286b5f1e
Julian <julian.w.francis@gmail.com>
2021-03-26 10:41:27 +0000

    Reorganised into PixelCNNDistribution and PixelCNNConditionalLayer

--- PixelCNNDistribution.py
+++ PixelCNNDistribution.py
@@ -30,45 +30,58 @@ class PixelCNNDiscreteMixLayer(nn.Module):
         return 30 if channels == 1 else 100
 
         
-class PixelCNNDistribution(nn.Module):
-    def __init__(self, event_shape, output_distribution_layer=PixelCNNDiscreteMixLayer()):
-        super(PixelCNNDistribution, self).__init__()
+class _PixelCNNDistribution(nn.Module):
+    def __init__(self, event_shape, output_distribution_layer=PixelCNNDiscreteMixLayer(), num_conditional=None):
+        super(_PixelCNNDistribution, self).__init__()
         self.pixelcnn_net = pixelcnn_model.PixelCNN(nr_resnet=5, nr_filters=160,
-                input_channels=event_shape[0], nr_params=output_distribution_layer.params_size(event_shape[0]))
+                input_channels=event_shape[0], nr_params=output_distribution_layer.params_size(event_shape[0]), nr_conditional=num_conditional)
         self.output_distribution_layer = output_distribution_layer
         self.event_shape = event_shape
 
-    def log_prob(self, samples):
-        params = self.pixelcnn_net((samples*2.0)-1.0)
+    def log_prob(self, samples, conditionals=None):
+        params = self.pixelcnn_net((samples*2.0)-1.0, conditional=conditionals)
         return self.output_distribution_layer(params).log_prob(samples)
 
-    def sample(self):
+    def sample(self, conditionals=None):
         with torch.no_grad():
             sampl = torch.zeros([1]+self.event_shape, device="cuda")
             for y in range(self.event_shape[1]):
                 for x in range(self.event_shape[2]):
-                    params=self.pixelcnn_net((sampl*2)-1,sample=True)
+                    params=self.pixelcnn_net((sampl*2)-1, sample=True, conditional=conditionals)
                     s = self.output_distribution_layer(params).sample()
                     sampl[0,:,y,x] = s[0,:,y,x]
         return sampl
 
 
-class PixelCNNConditionalDistribution(nn.Module):
-    def __init__(self, num_conditional):
-        super(PixelCNNConditionalDistribution, self).__init__()
-        self.pixelcnn_net = pixelcnn_model.PixelCNN(nr_resnet=5, nr_filters=160,
-                input_channels=3, nr_logistic_mix=100, nr_conditional=num_conditional)
+class PixelCNNDistribution(nn.Module):
+    def __init__(self, event_shape, output_distribution_layer=PixelCNNDiscreteMixLayer()):
+        super(PixelCNNDistribution, self).__init__()
+        self.distribution = _PixelCNNDistribution(event_shape, output_distribution_layer)
 
-    def log_prob(self, samples, conditionals):
-        distribution_params = self.pixelcnn_net((samples*2.0)-1.0, conditional=conditionals)
-        return {"log_prob": -pixelcnn_utils.discretized_mix_logistic_loss(samples, distribution_params)}
+    def log_prob(self, samples):
+        return self.distribution.log_prob(samples)
 
-    def sample(self, conditionals):
-        with torch.no_grad():
-            sampl = torch.zeros( (1,3,32,32), device="cuda")
-            for y in range(32):
-                for x in range(32):
-                    distribution_params=self.pixelcnn_net((sampl*2)-1,sample=True, conditional=conditionals)
-                    s = pixelcnn_utils.sample_from_discretized_mix_logistic(distribution_params, 100)
-                    sampl[0,:,y,x] = s[0,:,y,x]
-        return (sampl+1.0)/2.0
+    def sample(self):
+        return self.distribution.sample()
+
+
+class _PixelCNNLayerDistribution(nn.Module):
+    def __init__(self, distribution, params):
+        super(_PixelCNNLayerDistribution, self).__init__()
+        self.distribution = distribution
+        self.params = params
+
+    def log_prob(self, samples):
+        return self.distribution.log_prob(samples, self.params)
+
+    def sample(self):
+        return self.distribution.sample(self.params)
+
+
+class PixelCNNLayer(nn.Module):
+    def __init__(self, event_shape, num_conditional, output_distribution_layer=PixelCNNDiscreteMixLayer()):
+        super(PixelCNNLayer, self).__init__()
+        self.distribution = _PixelCNNDistribution(event_shape, output_distribution_layer, num_conditional)
+
+    def forward(self, x):
+        return _PixelCNNLayerDistribution(self.distribution, x)
