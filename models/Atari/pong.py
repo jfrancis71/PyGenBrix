e3db98a09c487e61b6f73226e7896cb6d2f15c02
Julian <julian.w.francis@gmail.com>
2021-08-03 16:56:18 +0100

    using argparse for stackframes and saving model

--- models/Atari/pong.py
+++ models/Atari/pong.py
@@ -2,10 +2,17 @@
 #2 Frame: 18, 15.7
 #4 Frame: 17.3, 17.2
 
-import gym
+import argparse
 
+import gym
 from stable_baselines3 import DQN
 from stable_baselines3.common.atari_wrappers import *
+
+
+ap = argparse.ArgumentParser(description="Pong DQN")
+ap.add_argument("--frame_stacks", default=4)
+ap.add_argument("--save_path")
+ns = ap.parse_args()
 
 
 class DisplayEnv(gym.Wrapper):
@@ -43,6 +50,8 @@ env = gym.make("PongNoFrameskip-v4")
 env.seed(42)
 env = MaxAndSkipEnv(env, skip=4)
 env = WarpFrame(env)
-env = StackFramesWrapper(env, 4, np.uint8)
+env = StackFramesWrapper(env, ns.frame_stacks, np.uint8)
 model = DQN('CnnPolicy', env, verbose=1, buffer_size=10000, learning_rate=.0001, learning_starts=100000, target_update_interval=1000)
 model.learn(total_timesteps=2000000)
+if ns.save_path is not None:
+    model.save(ns.save_path)
