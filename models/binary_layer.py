6a5905085b83f3ad2c641d5581ce205adda54deb
Julian <julian.w.francis@gmail.com>
2021-09-19 12:21:32 +0100

    Adding comment on source of idea

--- models/binary_layer.py
+++ models/binary_layer.py
@@ -1,6 +1,10 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+#Idea comes from Dreamer v2
+#https://arxiv.org/pdf/2010.02193.pdf, page 3
+#They used stochastic neuron, Following code is deterministic
+
 
 class BinaryLayer(nn.Module):
     def __init__(self):
