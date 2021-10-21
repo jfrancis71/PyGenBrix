778c8a890077a186dfe43bc11eb5f3bf3f56e765
Julian <julian.w.francis@gmail.com>
2021-10-21 14:59:16 +0100

    Consolidating PixelCNN and ParallelCNN into one trainer

--- models/generative_trainer.py
+++ models/generative_trainer.py
@@ -2,12 +2,13 @@ import argparse
 import torchvision
 import pytorch_lightning as pl
 
-import PyGenBrix.dist_layers.pixelcnn as cnn
+import PyGenBrix.dist_layers.pixelcnn as pixel_cnn
+import PyGenBrix.dist_layers.parallelcnn as parallel_cnn
 import PyGenBrix.Train as Train
 import PyGenBrix.dist_layers.common_layers as dl
 import PyGenBrix.dist_layers.spatial_independent as sp
 
-ap = argparse.ArgumentParser(description="PixelCNN")
+ap = argparse.ArgumentParser(description="generative_trainer")
 ap.add_argument("--tensorboard_log")
 ap.add_argument("--max_epochs", default=10, type=int)
 ap.add_argument("--nr_resnet", default=5, type=int)
@@ -15,7 +16,10 @@ ap.add_argument("--lr", default=.0002, type=float)
 ap.add_argument("--fast_dev_run", action="store_true")
 ap.add_argument("--dataset")
 ap.add_argument("--rv_distribution")
+ap.add_argument("--model")
+ap.add_argument("--parallelcnn_num_upsampling_stages", default=1, type=int)
 ns = ap.parse_args()
+
 if ns.dataset == "cifar10":
     dataset = torchvision.datasets.CIFAR10(root='/home/julian/ImageDataSets/CIFAR10', train=True,
         download=False, transform=torchvision.transforms.ToTensor())
@@ -41,16 +45,30 @@ elif ns.dataset == "mnist":
 else:
     print("Dataset not recognized.")
     quit()
+
 if ns.rv_distribution == "bernoulli":
     rv_distribution = dl.IndependentBernoulliLayer()
+elif ns.rv_distribution == "q3":
+    rv_distribution = dl.IndependentQuantizedLayer( num_buckets = 8)
 elif ns.rv_distribution == "spiq3":
     rv_distribution = sp.SpatialIndependentDistributionLayer( [image_channels, 32, 32], dl.IndependentQuantizedLayer( num_buckets = 8), num_params=30 )
 elif ns.rv_distribution == "PixelCNNDiscMixDistribution":
-    rv_distribution = cnn.PixelCNNDiscreteMixLayer()
+    rv_distribution = pixel_cnn.PixelCNNDiscreteMixLayer()
 else:
     print("rv distribution not recognized")
     quit()
-model = cnn.PixelCNNDistribution([ image_channels, image_size, image_size ], rv_distribution, nr_resnet=ns.nr_resnet )
+
+if ns.model == "PixelCNN":
+    model = pixel_cnn.PixelCNNDistribution([ image_channels, image_size, image_size ], rv_distribution, nr_resnet=ns.nr_resnet )
+elif ns.model == "ParallelCNN":
+    model = parallel_cnn.ParallelCNNDistribution([ image_channels, image_size, image_size ], rv_distribution, max_unet_layers=3, num_upsampling_stages=int(ns.parallelcnn_num_upsampling_stages))
+    if ns.rv_distribution == "spiq3":
+        print("parallelcnn and spiq3 incompatible")
+        quit()
+else:
+    print("model not recognized")
+    quit()
+
 trainer = Train.LightningDistributionTrainer( model, dataset, learning_rate = ns.lr, batch_size = 8 )
 pl.Trainer(fast_dev_run=ns.fast_dev_run, gpus=1, accumulate_grad_batches=8, max_epochs=ns.max_epochs, 
           callbacks=[
