b962a6ba568a6573c1288009f183e2667105e9fa
Julian <julian.w.francis@gmail.com>
2021-10-30 09:06:50 +0100

    Adding a PyGenBrix VDVAE distribution. generating_trainer now supports celeba64 and VDVAE and custom batch size/accumulate gradients from command line

---
+++ dist_layers/vdvae.py
@@ -0,0 +1,44 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import vdvae.hps as hps
+import vdvae.vae as vdvae
+
+h = hps.Hyperparams()
+h.width = 384
+h.zdim = 16
+h.dec_blocks = "1x1,4m1,4x2,8m4,8x5,16m8,16x10,32m16,32x21"
+h.enc_blocks = "32x11,32d2,16x6,16d2,8x6,8d2,4x3,4d4,1x3"
+h.custom_width_str = ""
+h.no_bias_above = 64
+h.bottleneck_multiple = 0.25
+h.num_mixtures = 10
+h.grad_clip = 200.0
+h.skip_threshold = 400.0
+
+
+class VDVAE(nn.Module):
+    def __init__(self, event_shape, rv_distribution):
+        super(VDVAE, self).__init__()
+        h.image_channels = event_shape[0]
+        h.image_size = event_shape[2]
+        if event_shape[2] == 32:
+            h.dec_blocks = "1x1,4m1,4x2,8m4,8x5,16m8,16x10,32m16,32x21"
+            h.enc_blocks = "32x11,32d2,16x6,16d2,8x6,8d2,4x3,4d4,1x3"
+        elif event_shape[2] == 64:
+            h.dec_blocks = "1x2,4m1,4x3,8m4,8x7,16m8,16x15,32m16,32x31,64m32,64x12"
+            h.enc_blocks = "64x11,64d2,32x20,32d2,16x9,16d2,8x8,8d2,4x7,4d4,1x5"
+        else:
+            print("VDVAE shape not standard.")
+            quit()
+        self.vae = vdvae.VAE(h, rv_distribution)
+        self.ndims = np.prod(event_shape)
+
+    def log_prob(self, samples):
+        samples = samples.permute(0, 2, 3, 1)
+        elbo = self.vae.forward((samples-.5)*4, samples)["elbo"]*self.ndims
+        return {"log_prob": -elbo}
+
+    def sample(self, temperature=1.0):
+        samples = torch.tensor(self.vae.forward_uncond_samples(1, t=temperature))
+        return samples
--- models/generative_trainer.py
+++ models/generative_trainer.py
@@ -4,6 +4,7 @@ import pytorch_lightning as pl
 
 import PyGenBrix.dist_layers.pixelcnn as pixel_cnn
 import PyGenBrix.dist_layers.parallelcnn as parallel_cnn
+import PyGenBrix.dist_layers.vdvae as vdvae
 import PyGenBrix.Train as Train
 import PyGenBrix.dist_layers.common_layers as dl
 import PyGenBrix.dist_layers.spatial_independent as sp
@@ -11,13 +12,16 @@ import PyGenBrix.dist_layers.spatial_independent as sp
 ap = argparse.ArgumentParser(description="generative_trainer")
 ap.add_argument("--tensorboard_log")
 ap.add_argument("--max_epochs", default=10, type=int)
-ap.add_argument("--nr_resnet", default=5, type=int)
 ap.add_argument("--lr", default=.0002, type=float)
+ap.add_argument("--batch_size", default=8, type=int)
+ap.add_argument("--accumulate_grad_batches", default=8, type=int)
 ap.add_argument("--fast_dev_run", action="store_true")
 ap.add_argument("--dataset")
 ap.add_argument("--rv_distribution")
 ap.add_argument("--model")
 ap.add_argument("--parallelcnn_num_upsampling_stages", default=1, type=int)
+ap.add_argument("--nr_resnet", default=5, type=int)
+ap.add_argument("--train_log_freq", default=0, type=int)
 ns = ap.parse_args()
 
 if ns.dataset == "cifar10":
@@ -33,7 +37,15 @@ elif ns.dataset == "celeba32":
         ]))
     image_channels = 3
     image_size = 32
-elif ns.dataset == "mnist":
+elif ns.dataset == "celeba64":
+    dataset = torchvision.datasets.ImageFolder(root="/home/julian/ImageDataSets/celeba/",
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.Pad((-15, -40,-15-1, -30-1)),
+            torchvision.transforms.Resize(64), torchvision.transforms.ToTensor(),
+        ]))
+    image_channels = 3
+    image_size = 64
+elif ns.dataset == "mnist32":
     dataset = torchvision.datasets.MNIST('/home/julian/ImageDataSets/MNIST',
     train=True, download=False,
     transform=torchvision.transforms.Compose([
@@ -51,7 +63,7 @@ if ns.rv_distribution == "bernoulli":
 elif ns.rv_distribution == "q3":
     rv_distribution = dl.IndependentQuantizedLayer( num_buckets = 8)
 elif ns.rv_distribution == "spiq3":
-    rv_distribution = sp.SpatialIndependentDistributionLayer( [image_channels, 32, 32], dl.IndependentQuantizedLayer( num_buckets = 8), num_params=30 )
+    rv_distribution = sp.SpatialIndependentDistributionLayer( [image_channels, image_size, image_size], dl.IndependentQuantizedLayer( num_buckets = 8), num_params=30 )
 elif ns.rv_distribution == "PixelCNNDiscMixDistribution":
     rv_distribution = pixel_cnn.PixelCNNDiscreteMixLayer()
 else:
@@ -59,18 +71,20 @@ else:
     quit()
 
 if ns.model == "PixelCNN":
-    model = pixel_cnn.PixelCNNDistribution([ image_channels, image_size, image_size ], rv_distribution, nr_resnet=ns.nr_resnet )
+    model = pixel_cnn.PixelCNNDistribution([image_channels, image_size, image_size], rv_distribution, nr_resnet=ns.nr_resnet )
 elif ns.model == "ParallelCNN":
-    model = parallel_cnn.ParallelCNNDistribution([ image_channels, image_size, image_size ], rv_distribution, max_unet_layers=3, num_upsampling_stages=int(ns.parallelcnn_num_upsampling_stages))
+    model = parallel_cnn.ParallelCNNDistribution([image_channels, image_size, image_size], rv_distribution, max_unet_layers=3, num_upsampling_stages=int(ns.parallelcnn_num_upsampling_stages))
     if ns.rv_distribution == "spiq3":
         print("parallelcnn and spiq3 incompatible")
         quit()
+elif ns.model == "VDVAE":
+    model = vdvae.VDVAE([image_channels, image_size, image_size], rv_distribution)
 else:
     print("model not recognized")
     quit()
 
-trainer = Train.LightningDistributionTrainer( model, dataset, learning_rate = ns.lr, batch_size = 8 )
-pl.Trainer(fast_dev_run=ns.fast_dev_run, gpus=1, accumulate_grad_batches=8, max_epochs=ns.max_epochs, 
-          callbacks=[
-                     Train.LogSamplesEpochCallback(temperature=1.0)
-          ], default_root_dir=ns.tensorboard_log).fit(trainer)
+trainer = Train.LightningDistributionTrainer(model, dataset, learning_rate=ns.lr, batch_size=ns.batch_size)
+train_callback = Train.LogSamplesTrainingCallback(every_global_step=ns.train_log_freq, temperature=1.0) if ns.train_log_freq != 0 else None
+pl.Trainer(fast_dev_run=ns.fast_dev_run, gpus=1, accumulate_grad_batches=ns.accumulate_grad_batches, max_epochs=ns.max_epochs, 
+           callbacks=[Train.LogSamplesEpochCallback(temperature=1.0), train_callback],
+           default_root_dir=ns.tensorboard_log).fit(trainer)
