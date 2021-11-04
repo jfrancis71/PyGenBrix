9de590dde890310e8ca21cdf86b5a1482fc75704
Julian <julian.w.francis@gmail.com>
2021-11-04 11:42:06 +0000

    Uncoupling vdvae_train from directly running vdvae functions to going through the PyGenBrix distributions

--- models/vdvae_train.py
+++ models/vdvae_train.py
@@ -6,28 +6,30 @@ import torchvision
 import pytorch_lightning as pl
 import vdvae.train as vdvae_train
 import vdvae.hps as hps
-import vdvae.vae as vdvae
+import PyGenBrix.dist_layers.vdvae as vdvae
 import pygenbrix_layer as pygl
 import PyGenBrix.dist_layers.common_layers as dl
 import PyGenBrix.dist_layers.spatial_independent as sp
 import PyGenBrix.models.parser as parser
 
 
-class VDVAEModel(nn.Module):
-    def __init__(self, vae, ema_vae):
-        super(VDVAEModel, self).__init__()
-        self.vae, self.ema_vae = vae, ema_vae
+class EMAModel(nn.Module):
+    def __init__(self, model, ema_model):
+        super(EMAModel, self).__init__()
+        self.model, self.ema_model = model, ema_model
 
 
-class VDVAETrainer(pl.LightningModule):
+class EMATrainer(pl.LightningModule):
     def __init__(self, model, dataset, batch_size=8, learning_rate=0.0002):
-        super(VDVAETrainer, self).__init__()
+        super(EMATrainer, self).__init__()
         self.model = model
         self.dataset = dataset
         self.train_set, self.val_set = self.get_datasets()
         self.automatic_optimization = False
         self.batch_size = batch_size
         self.learning_rate = learning_rate
+        self.grad_clip = 200
+        self.skip_threshold = 400
 
     def get_datasets(self):
         dataset_size = len(self.dataset)
@@ -39,19 +41,27 @@ class VDVAETrainer(pl.LightningModule):
 
     def training_step(self, batch, batch_indx):
         x, y = batch
-        x = x.permute(0, 2, 3, 1)
-        stats = vdvae_train.training_step(h, (x-.5)*4, x, self.model.vae, self.model.ema_vae, self.optimizers(), -1)
-        self.log('log_prob', -stats["elbo"], on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('kl', stats["rate"], on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('recon_error', stats["distortion"], on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        ndims = np.prod(x.shape[1:])
+        self.model.model.zero_grad()
+        log_prob = self.model.model.log_prob(x)["log_prob"].mean()
+        log_prob_per_pixel = log_prob/ndims
+        loss = -log_prob_per_pixel
+        loss.backward()       
+        grad_norm = torch.nn.utils.clip_grad_norm_(self.model.model.parameters(), self.grad_clip).item()
+        log_prob_per_pixel_nans = torch.isnan(log_prob_per_pixel)
+        self.log("log_prob_nans", log_prob_per_pixel_nans, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        skipped_update = 1
+        if (log_prob_per_pixel_nans == False and grad_norm < self.skip_threshold):
+            self.optimizers().step()
+            skipped_update = 0
+        self.log('skipped_update', skipped_update, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log('grad_norm', grad_norm, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log('log_prob', log_prob_per_pixel, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
     def validation_step(self, batch, batch_indx):
         x, y = batch
-        x = x.permute(0, 2, 3, 1)
-        stats = vdvae_train.eval_step((x-.5)*4, x, self.model.ema_vae)
-        self.log("validation_log_prob", -stats["elbo"], on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        self.log("validation_kl", stats["rate"], on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        self.log("validation_recon_error", stats["distortion"], on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        log_prob = self.model.ema_model.log_prob(x)
+        self.log("validation_log_prob", log_prob["log_prob"], on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.model.parameters(), lr = self.learning_rate)
@@ -70,12 +80,12 @@ class LogSamplesVAECallback(pl.Callback):
 
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
         if (pl_module.global_step % self.step_freq == 0) and (batch_idx % trainer.accumulate_grad_batches == 0):
-            samples = pl_module.model.ema_vae.forward_uncond_samples(8, t=1.0)
+            samples = pl_module.model.ema_model.sample([8], temperature=1.0)
             samples_grid = torchvision.utils.make_grid(samples, padding=10, nrow=4)
             pl_module.logger.experiment.add_image("train_sample", samples_grid, pl_module.global_step, dataformats="CHW")
 
 
-ap = argparse.ArgumentParser(description="VDVAE")
+ap = argparse.ArgumentParser(description="EMATrainer")
 ap.add_argument("--tensorboard_log")
 ap.add_argument("--max_epochs", default=10, type=int)
 ap.add_argument("--lr", default=.0002, type=float)
@@ -106,10 +116,10 @@ if ns.model != "vdvae":
     quit()
 h.image_channels = event_shape[0]
 h.width = event_shape[2]
-vae = vdvae.VAE(h, rv_distribution)
-ema_vae = vdvae.VAE(h, rv_distribution)
+vae = vdvae.VDVAE(event_shape, rv_distribution)
+ema_vae = vdvae.VDVAE(event_shape, rv_distribution)
 ema_vae.requires_grad = False
-model = VDVAEModel(vae, ema_vae)
-trainer = VDVAETrainer(model, dataset, batch_size=8, learning_rate=ns.lr)
+model = EMAModel(vae, ema_vae)
+trainer = EMATrainer(model, dataset, batch_size=8, learning_rate=ns.lr)
 pl.Trainer(fast_dev_run = ns.fast_dev_run, gpus=1, accumulate_grad_batches = 1, max_epochs=ns.max_epochs, default_root_dir=ns.tensorboard_log, 
     callbacks=[LogSamplesVAECallback(1000)]).fit(trainer)
