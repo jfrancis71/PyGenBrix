f282ec381b8f7c7b03915b790afb576b6dd84d44
Julian <julian.w.francis@gmail.com>
2022-06-06 09:23:12 +0100

    Updating to be similar to pfrl_train

--- models/Atari/pfrl_rnn.py
+++ models/Atari/pfrl_rnn.py
@@ -21,6 +21,7 @@ ap.add_argument("--model")
 ap.add_argument("--demo", action="store_true")
 ap.add_argument("--sleep", default=.02, type=float)
 ap.add_argument("--env", default="PongNoFrameskip-v4")
+ap.add_argument("--steps", default=1e7, type=int)
 ns = ap.parse_args()
 
 env = atari_wrappers.wrap_deepmind(
@@ -29,8 +30,14 @@ env = atari_wrappers.wrap_deepmind(
     clip_rewards=True,
     frame_stack = False
 )
+test_env = atari_wrappers.wrap_deepmind(
+    atari_wrappers.make_atari(ns.env, max_frames=10000),
+    episode_life=False,
+    clip_rewards=False,
+    frame_stack = False
+)
 
-n_actions = env.action_space.n
+n_actions = test_env.action_space.n
 q_func = pfrl.nn.RecurrentSequential(
     nn.Conv2d(1, 32, 8, stride=4),
     nn.ReLU(),
@@ -49,24 +56,33 @@ replay_buffer = pfrl.replay_buffers.EpisodicReplayBuffer(10**6)
 explorer = explorers.LinearDecayEpsilonGreedy(
     1.0,
     .01,
-    1000000,
+    ns.steps,
     lambda: numpy.random.randint(n_actions),
 )
 
 def phi(x):
     return numpy.asarray(x, dtype=numpy.float32) / 255
 
-optimizer = torch.optim.Adam(q_func.parameters(), lr=1e-4, eps=1e-4)
+optimizer = pfrl.optimizers.RMSpropEpsInsideSqrt(
+    q_func.parameters(),
+    lr=2.5e-4,
+    alpha=0.95,
+    momentum=0.0,
+    eps=1e-2,
+    centered=True,
+)
 
-agent = pfrl.agents.DQN(
+agent = pfrl.agents.DoubleDQN(
     q_func,
     optimizer,
     replay_buffer,
     gamma=0.99,
     explorer=explorer,
-    replay_start_size=10000,
-    target_update_interval=1000,
-    update_interval=1,
+    replay_start_size=5*10**4,
+    target_update_interval=10**4,
+    clip_delta=True,
+    update_interval=4,
+    batch_accumulator="sum",
     phi=phi,
     gpu=0,
     episodic_update_len=10,
@@ -75,18 +91,18 @@ agent = pfrl.agents.DQN(
 
 if ns.demo:
     agent.load(ns.model)
-    env = pfrl.wrappers.Render(env)
-    env = sleep_wrapper.SleepWrapper(env, ns.sleep)
-    experiments.eval_performance(env=env, agent=agent, n_steps=100000, n_episodes=None)
+    test_env = pfrl.wrappers.Render(test_env)
+    test_env = sleep_wrapper.SleepWrapper(test_env, ns.sleep)
+    experiments.eval_performance(env=test_env, agent=agent, n_steps=100000, n_episodes=None)
 else:
     experiments.train_agent_with_evaluation(
         agent=agent,
         env=env,
-        steps=1000000,
+        steps=ns.steps,
         eval_n_steps=None,
         checkpoint_freq=None,
         eval_n_episodes=10,
         eval_interval=100000,
         outdir=ns.model,
-        eval_env=env,
+        eval_env=test_env,
         use_tensorboard=True)
