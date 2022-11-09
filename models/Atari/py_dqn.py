9ec5d966d8f0e74fc32a5bf433ebb7e0902d2f6d
Julian <julian.w.francis@gmail.com>
2022-11-09 09:56:14 +0000

    Adding ReplayBuffer classes

--- models/Atari/py_dqn.py
+++ models/Atari/py_dqn.py
@@ -4,6 +4,9 @@
 # results on Pong in 500,000 steps.
 
 
+import collections
+import random
+from collections import namedtuple
 import numpy as np
 import torch
 import torch.nn as nn
@@ -21,10 +24,49 @@ def phi(x):
     return np.asarray(x, dtype=np.float32) / 255
 
 
+class PFRLReplayBuffer():
+    def __init__(self):
+        self.replay_buffer = pfrl.replay_buffers.ReplayBuffer(capacity=15000)
+
+    def append(self, observation, action, reward, next_observation):
+        self.replay_buffer.append(observation, action, reward, next_observation)
+
+    def sample(self, batch_size):
+        sample_buffer = self.replay_buffer.sample(batch_size)
+        states = np.array([[transition["state"] for transition in batch] for batch in sample_buffer])
+        actions = np.array([[transition["action"] for transition in batch] for batch in sample_buffer])
+        rewards = np.array([[transition["reward"] for transition in batch] for batch in sample_buffer])
+        next_states = np.array([[transition["next_state"] for transition in batch] for batch in sample_buffer])
+        return states, actions, rewards, next_states
+
+
+class ReplayBuffer():
+    def __init__(self):
+        self.states = collections.deque(maxlen=15000)
+        self.actions = collections.deque(maxlen=15000)
+        self.rewards = collections.deque(maxlen=15000)
+        self.next_states = collections.deque(maxlen=15000)
+
+    def append(self, state, action, reward, next_state):
+        self.states.append(state)
+        self.actions.append(action)
+        self.rewards.append(reward)
+        self.next_states.append(next_state)
+
+    def sample(self, batch_size):
+        chosen_transitions = random.sample(range(0, len(self.states) - 1), batch_size)
+        states = np.array([[self.states[t]] for t in chosen_transitions])
+        actions = np.array([[self.actions[t]] for t in chosen_transitions])
+        rewards = np.array([[self.rewards[t]] for t in chosen_transitions])
+        next_states = np.array([[self.next_states[t]] for t in chosen_transitions])
+        return states, actions, rewards, next_states
+
+
 class PyDQNAgent(nn.Module):
     def __init__(self, n_actions, tb_writer, max_steps):
         super(PyDQNAgent, self).__init__()
-        self.replay_buffer = pfrl.replay_buffers.ReplayBuffer(capacity=15000)
+#        self.replay_buffer = PFRLReplayBuffer()
+        self.replay_buffer = ReplayBuffer()
         self.steps = 0
         self.target_update_interval = 1000
         if max_steps == 0:
@@ -62,14 +104,7 @@ class PyDQNAgent(nn.Module):
     def sample_and_improve(self, batch_size):
         if self.steps > 10001:
             sample_buffer = self.replay_buffer.sample(batch_size)
-            exp_batch = batch_experiences(
-                sample_buffer,
-                device="cuda",
-                phi=phi,
-                gamma=.99,
-                batch_states=batch_states,
-            )
-            loss = self.loss(exp_batch)
+            loss = self.loss(sample_buffer)
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
@@ -77,9 +112,13 @@ class PyDQNAgent(nn.Module):
             self.target_nn.load_state_dict(self.moving_nn.state_dict())
 
     def loss(self, exp_batch):
-        curr_state_action_value = self.moving_nn(exp_batch["state"]).gather(1,exp_batch["action"][:,None]).squeeze(-1)
-        next_state_action_value = self.target_nn(exp_batch["next_state"]).max(1)[0].detach()
-        q_target = exp_batch["reward"] + .99 * next_state_action_value
+        states = torch.tensor(exp_batch[0], device="cuda", dtype=torch.float32)/255.0
+        actions = torch.tensor(exp_batch[1], device="cuda")
+        rewards = torch.tensor(exp_batch[2], device="cuda", dtype=torch.float32)
+        next_states = torch.tensor(exp_batch[3], device="cuda", dtype=torch.float32)/255.0
+        curr_state_action_value = self.moving_nn(states[:,0]).gather(1,actions[:,:1])[:,0]
+        next_state_action_value = self.target_nn(next_states[:,0]).max(1)[0].detach()
+        q_target = rewards[:,0] + .99 * next_state_action_value
         return nn.MSELoss()(curr_state_action_value, q_target)
 
     def save(self, filename):
