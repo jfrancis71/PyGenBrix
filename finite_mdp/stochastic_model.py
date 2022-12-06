ead8a1f636ee92396b00fb33c0e9babc5f268b00
Julian <julian.w.francis@gmail.com>
2022-12-06 08:40:28 +0000

    Commenting and removing redundant code

--- finite_mdp/stochastic_model.py
+++ finite_mdp/stochastic_model.py
@@ -2,11 +2,29 @@ import random
 import copy
 import numpy as np
 import q_model
+import torch
 
 
 class QLearnableStochasticModel:
+    """Learns a probability distribution over MDP 's and stores a current MDP.
+
+    Args:
+    env: environment, is purely used to map states<->integers and retrieve environment width and height.
+
+    State transitions are assumed stochastic, modelled as categorical distributions. Rewards, Dones are assumed
+    deterministic, so once they have been observed, they are known.
+
+    When sampling a new MDP:
+
+    The state transition distributions are sampled from Dirichlet distributions where concentration parameters are
+    counts of observed state transitions. Unknown rewards are assumed optimistic (+100). Unknown done's are bernoulli
+    sampled with probability (0.5, 0.5)
+    """
+    # Inspiration:
+    # Bayes Approach for Planning and Learning in Partially Observable Markov Decision Process,
+    # Ross, Pineau, Chaib-Draa, Kreitmann, 2011
     def __init__(self, env):
-        self.env = copy.deepcopy(env)
+        self.env = env  # Only used for the integer to state, state to integer functions
         self.height, self.width = self.env.height, self.env.width
         self.dones = np.zeros([self.height, self.width, 4]) - 1
         self.rewards = np.zeros([self.height, self.width, 4])
@@ -17,19 +35,21 @@ class QLearnableStochasticModel:
         self.sample_parameters()
 
     def sample(self, observation, action):
-        self.env.current_state = observation.copy()
+        """Sample a new state, reward, done from the currently sampled MDP given the observation and action."""
         done = self.dones[observation[0], observation[1], action]
         reward = self.rewards[observation[0], observation[1], action]
         observation_state = self.env.state_to_integer(observation)
         next_state = np.random.choice(self.num_states, p=self.state_transition_cat_probs[observation_state][action])
-        actual_new_observation, actual_reward, actual_done, info = self.env.step(action)
         if self.visits[observation[0], observation[1], action] == 0:
             reward = self.random_rewards[observation[0], observation[1], action]
             done = self.random_dones[observation[0], observation[1], action]
         new_observation = self.env.integer_to_state(next_state)
-        return new_observation, reward, done, info
+        return new_observation, reward, done, None
 
     def observe(self, observation, action, reward, done, next_observation):
+        """Update the distribution over MDP's using this state transition.
+
+        Note: does not affect current MDP"""
         self.dones[observation[0], observation[1], action] = done
         self.visits[observation[0], observation[1], action] = 1
         self.rewards[observation[0], observation[1], action] = reward
@@ -38,6 +58,7 @@ class QLearnableStochasticModel:
         self.state_transitions_dirichlet_alpha[observation_state, action, next_observation_state] += 1000
 
     def sample_parameters(self):
+        """Resample a new MDP using the distribution over MDP's."""
         self.random_dones = np.random.binomial(1, p=np.ones([self.height, self.width, 4]) - .5)
         self.random_rewards = np.random.random([self.height, self.width, 4])+50
         self.state_transition_cat_probs =\
@@ -97,6 +118,10 @@ class QLearnableStochasticModel:
 
 
 class StochasticLearnableModelAgent:
+    """Provides an agent interface.
+
+    Learns a probability distribution over state transitions probabilities and deterministic reward and done functions.
+    """
     def __init__(self, env):
         self.action = None
         self.observation = None
@@ -105,6 +130,7 @@ class StochasticLearnableModelAgent:
         self.steps = 0
 
     def act(self, observation):
+        """return best action from current MDP given current observation"""
         q = self.q_algorithm.q[observation[0], observation[1]]
         action = q.argmax()
         self.action = action
@@ -113,7 +139,11 @@ class StochasticLearnableModelAgent:
         return action
 
     def observe(self, observation, reward, done, _):
+        """Update determistic functions reward and done, and update probability distribution over state transition
+        probability distributions. Intermittently resample an MDP using the most optimistic (with respect to the
+        current observation) of a sample of MDP's."""
         self.learnable_model.observe(self.observation, self.action, reward, done, observation)
+        # Could consider replanning if new observations substantially change the probability distributions over MDP's,
         if self.steps % 20 == 0:
             print("Total explored=", self.learnable_model.visits.sum())
             best_q = -1000000
