9659d46205e83a765594396bded2256df552dc10
Julian <julian.w.francis@gmail.com>
2023-02-02 10:41:44 +0000

    Adding Thompson-Sampling version, implimented using Dirichlet Distribution

--- misc/connect4.py
+++ misc/connect4.py
@@ -12,6 +12,7 @@ def get_legal_actions(env):
     action_mask = observation["action_mask"]
     return np.nonzero(action_mask)[0]
 
+
 def select_random_action(env):
     legal_actions = get_legal_actions(env)
     num_actions = len(legal_actions)
@@ -22,6 +23,7 @@ class MCTSNode:
     def __init__(self, parent, env):
         self.env = env
         self.sum_value = 0
+        self.counts = [0, 0, 0]  # counts of loss, draw, win
         self.count = 0
         self.children = {}  # is a leaf node by default
         self.legal_actions = get_legal_actions(env)
@@ -30,6 +32,7 @@ class MCTSNode:
         self.terminated = termination
         self.winner = reward
         self.max_depth = 0
+
 
 def random_rollout(node):
     """does not alter env returns termination reward from perspective of current player"""
@@ -45,15 +48,24 @@ def random_rollout(node):
     return reward*swap_reward
 
 
-def select_node(start_node):
+def select_node(start_node, tree_selection_policy):
     # ucb rank the children
     if start_node.count == 0:
         return start_node
-    log_parent_n = math.log(start_node.count)
+
+    selected_node = tree_selection_policy(start_node)
+    if selected_node is None:
+        return start_node
+    else:
+        return select_node(selected_node, tree_selection_policy)
+
+
+def tree_selection_policy_ucb1(node):
+    log_parent_n = math.log(node.count)
     best_ucb = -100
     c = math.sqrt(2.0)
     selected_node = None
-    for child in start_node.children.values():
+    for child in node.children.values():
         if child.count == 0:
             ucb = 1000.0
         else:
@@ -61,10 +73,20 @@ def select_node(start_node):
         if ucb > best_ucb:
             best_ucb = ucb
             selected_node = child
-    if selected_node is None:
-        return start_node
-    else:
-        return select_node(selected_node)
+    return selected_node
+
+
+def tree_selection_policy_ts(node):
+    best_sample_value = -1000.0
+    selected_node = None
+    for child in node.children.values():
+        cat_dist = np.random.dirichlet([1+child.counts[0], 1+child.counts[1], 1+child.counts[2]])
+        sample = cat_dist[0]*-1 + cat_dist[1]*0 + cat_dist[2]*1
+        if sample > best_sample_value:
+            best_sample_value = sample
+            selected_node = child
+    return selected_node
+
 
 def expand_node(node):
     assert node.children == {}  # we only expand leaf nodes
@@ -74,26 +96,32 @@ def expand_node(node):
         node.children[legal_action] = MCTSNode(node, child_env)
     return list(node.children.values())[0]  # returns first of these children
 
+
 def backup_node(node):
     node = node.parent
     if node is None:
         return
     node.sum_value = 0
+    node.counts = [0, 0, 0]
     node.count = 0
     max_depth = -1
     for child in node.children.values():
         node.sum_value -= child.sum_value
         node.count += child.count
+        node.counts[0] += child.counts[2]
+        node.counts[2] += child.counts[0]
+        node.counts[1] += child.counts[1]
         if node.max_depth > max_depth:
             max_depth = child.max_depth
     node.max_depth = max_depth + 1
     backup_node(node)
 
-def mcts(env, num_iterations):
+
+def mcts(env, num_iterations, tree_selection_policy):
     root_node = MCTSNode(None, env)
 
     for iterations in range(num_iterations):
-        expanding_node = select_node(root_node)
+        expanding_node = select_node(root_node, tree_selection_policy)
         if expanding_node.terminated == 0:
             rollout_node = expand_node(expanding_node)
             if rollout_node.terminated:
@@ -103,6 +131,15 @@ def mcts(env, num_iterations):
         else:
             reward = expanding_node.winner
         expanding_node.sum_value += reward
+        if reward == 1:
+            expanding_node.counts[2] += 1
+        elif reward == -1:
+            expanding_node.counts[0] += 1
+        elif reward == 0:
+            expanding_node.counts[1] += 1
+        else:
+            print("Error")
+            quit()
         expanding_node.count += 1
         backup_node(expanding_node)
 
@@ -120,7 +157,7 @@ def mcts(env, num_iterations):
 
     print("Max Depth=", max_depth)
     for p in root_node.children.items():
-        print("Node ", p[0], "sum=", p[1].sum_value, " count = ", p[1].count)
+        print("Node ", p[0], "sum=", p[1].sum_value, " count = ", p[1].count, " counts=", p[1].counts)
     return max_value, best_move
 
 
@@ -152,8 +189,9 @@ def board_eval_move(env, depth, cached_evaluations):
 
     return best_score, best_move
 
+
 def print_board(env):
-    board = np.reshape(env.env.board, [6,7])
+    board = np.reshape(env.env.board, [6, 7])
     for level in board:
         print(*level, sep=' ')
     print("")
@@ -193,11 +231,12 @@ class MinimaxAgent:
 
 
 class MCTSAgent:
-    def __init__(self, num_iterations):
+    def __init__(self, num_iterations, agent_tree_policy):
         self.num_iterations = num_iterations
+        self.agent_tree_policy = agent_tree_policy
 
     def act(self, sim_env):
-        predicted_score, move = mcts(sim_env, self.num_iterations)
+        predicted_score, move = mcts(sim_env, self.num_iterations, self.agent_tree_policy)
         return predicted_score, move
 
 
@@ -211,7 +250,7 @@ def play_game(agent1, agent2):
     dual_env.reset()
     winner = 0
     termination = False
-    while termination == False:
+    while termination is False:
         print_board(dual_env.env)
         print("Agent1 Move\n")
         sim_env = dual_env.copy()
@@ -242,6 +281,7 @@ def play_game(agent1, agent2):
     time.sleep(10)
     return winner
 
+
 def play_tournament(agent1, agent2):
     games = []
     for i in range(10):
@@ -250,6 +290,7 @@ def play_tournament(agent1, agent2):
         games.append(winner)
         print("Games=", games)
     print("Tournament ", games)
+
 
 dual_env = DualEnvironment(connect_four_v3.env)
 
@@ -260,14 +301,32 @@ ap.add_argument("--agent1_minimax_depth", default=3, type=int)
 ap.add_argument("--agent2_minimax_depth", default=3, type=int)
 ap.add_argument("--agent1_mcts_num_iterations", default=500, type=int)
 ap.add_argument("--agent2_mcts_num_iterations", default=500, type=int)
+ap.add_argument("--agent1_tree_policy")
+ap.add_argument("--agent2_tree_policy")
 ns = ap.parse_args()
+
+if ns.agent1_tree_policy == "ucb1":
+    agent1_tree_policy = tree_selection_policy_ucb1
+elif ns.agent1_tree_policy == "ts":
+    agent1_tree_policy = tree_selection_policy_ts
+else:
+    print("Unknown tree policy for agent1")
+    quit()
+if ns.agent2_tree_policy == "ucb1":
+    agent2_tree_policy = tree_selection_policy_ucb1
+elif ns.agent2_tree_policy == "ts":
+    agent2_tree_policy = tree_selection_policy_ts
+else:
+    print("Unknown tree policy for agent1")
+    quit()
+
 
 if ns.agent1 == "human":
     agent1 = HumanAgent()
 elif ns.agent1 == "minimax":
     agent1 = MinimaxAgent(ns.agent1_minimax_depth)
 elif ns.agent1 == "mcts":
-    agent1 = MCTSAgent(ns.agent1_mcts_num_iterations)
+    agent1 = MCTSAgent(ns.agent1_mcts_num_iterations, agent1_tree_policy)
 else:
     print("Unknown agent1")
     quit()
@@ -277,7 +336,7 @@ if ns.agent2 == "human":
 elif ns.agent2 == "minimax":
     agent2 = MinimaxAgent(ns.agent2_minimax_depth)
 elif ns.agent2 == "mcts":
-    agent2 = MCTSAgent(ns.agent2_mcts_num_iterations)
+    agent2 = MCTSAgent(ns.agent2_mcts_num_iterations, agent2_tree_policy)
 else:
     print("Unknown agent2")
     quit()
