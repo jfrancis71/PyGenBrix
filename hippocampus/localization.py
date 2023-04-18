0b1763937abcb57556056397bc5eb3c13a9d209c
Julian <julian.w.francis@gmail.com>
2023-04-18 14:48:21 +0100

    adding HMM and plot

--- hippocampus/localization.py
+++ hippocampus/localization.py
@@ -29,6 +29,17 @@ class SingleFolderImage():
         return tensor_image1, tensor_image2
 
 
+class HMM:
+    def __init__(self):
+        self.state_distribution = torch.ones([35])/35
+        self.transition_matrix = 0.09*torch.eye(35) + .8*torch.roll(torch.eye(35), 1,1) + .09*torch.roll(torch.eye(35), 2,1) + .01*torch.ones([35,35])
+        
+    def observe(self, observation_values):  # I'm taking these as outputs of neural net, so not normalised probabilities
+        observation_vector = np.exp(-observation_values/500.)
+        unnormalized = torch.matmul(self.state_distribution, self.transition_matrix)*observation_vector
+        self.state_distribution = unnormalized/torch.sum(unnormalized)
+
+
 ap = argparse.ArgumentParser(description="gather_data")
 ap.add_argument("--ip_address") # ip address of Pi
 ap.add_argument("--folder", default="data")
@@ -41,11 +52,23 @@ conn = rpyc.classic.connect(ns.ip_address)
 conn.execute("from ev3dev2.motor import LargeMotor, OUTPUT_A")
 conn.execute("m = LargeMotor(OUTPUT_A)")
 seq = 0
+hmm = HMM()
+y_pos = np.arange(35)
+plt.ion()
+figure, ax = plt.subplots(figsize=(6, 4))
+performance = [1]*35
+ln = ax.bar(y_pos, performance, align='center', alpha=0.5)
 while True:
+    print("START IMPORT")
     image = ds.import_http_image("http://"+ns.ip_address+":8080?action=snapshot")
+    print("END IMPORT")
     image_tensor=torchvision.transforms.Compose([torchvision.transforms.Resize((32,32)), torchvision.transforms.ToTensor()])(image)
     representation = scene(image_tensor[:1])
-    res = np.array([torch.sum((representation-representations[po])**2) for po in range(35)])
-    print(res.argmin())
+    observation_values = np.array([torch.sum((representation-representations[po])**2) for po in range(35)])
+    hmm.observe(observation_values)
+    for i in range(35):
+        ln[i].set_height(hmm.state_distribution[i])
+        figure.canvas.draw()
+        figure.canvas.flush_events()
     conn.execute("m.on_for_rotations(75, 2)")
     seq += 1
