88e6222db96c9aba16542b2caef776b65f4a294e
Julian <julian.w.francis@gmail.com>
2023-11-15 17:28:05 +0000

    Correction of spelling mistake

--- thomas/thomas_server/camera.sh
+++ thomas/thomas_server/camera.sh
@@ -1,3 +1,3 @@
 # Must be run in conda RoboStack environment.
-ros2 run image_transport republiscompressed raw --ros-args --remap in/compressed:=/thomas/compressed --remap out:=server/raw
+ros2 run image_transport republish compressed raw --ros-args --remap in/compressed:=/thomas/compressed --remap out:=server/raw
 
