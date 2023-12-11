b78f1138bb2d077a98132ba944367490a8ac171b
Julian <julian.w.francis@gmail.com>
2023-12-11 14:50:53 +0000

    adding boxes for object_detector, and new person_follower

--- thomas/server/object_detector.py
+++ thomas/server/object_detector.py
@@ -7,6 +7,11 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
+from vision_msgs.msg import BoundingBox2D
+from vision_msgs.msg import ObjectHypothesis
+from vision_msgs.msg import ObjectHypothesisWithPose
+from vision_msgs.msg import Detection2D
+from vision_msgs.msg import Detection2DArray
 from cv_bridge import CvBridge
 
 
@@ -18,24 +23,53 @@ class ImageSubscriber(Node):
             in_channel,
             self.listener_callback,
             10)
-        self.publisher = self.create_publisher(String, "object_detection_messages", 10)
+        self.publisher = self.create_publisher(Detection2DArray, "object_detection_messages", 10)
+        self.render_image_publisher = self.create_publisher(Image, "render_image", 10)
         self.bridge = CvBridge()
         self.device = device
 
     def listener_callback(self, msg):
-        image = self.bridge.imgmsg_to_cv2(msg)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = image.transpose((2, 0, 1))
+        cv_image = self.bridge.imgmsg_to_cv2(msg)
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+        image = cv_image.copy().transpose((2, 0, 1))
         image = np.expand_dims(image, axis=0)
         image = image / 255.0
         image = torch.FloatTensor(image)
         image = image.to(self.device)
         detections = model(image)[0]
-        labels = [class_labels[id] for id, score in zip(detections["labels"], detections["scores"]) if score > .9]
-        msg = String()
-        msg.data = ','.join(labels)
-        self.publisher.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+#        labels = [class_labels[id] for id, score in zip(detections["labels"], detections["scores"]) if score > .9]
+#        msg = String()
+#        msg.data = ','.join(labels)
+#        self.publisher.publish(msg)
+#        self.get_logger().info('Publishing: "%s"' % msg.data)
+        print("det=", detections)
+        det_array = Detection2DArray()
+        for label_id, score, box in zip(detections["labels"], detections["scores"], detections["boxes"]):
+            if score < .9:
+                continue
+            box = box.detach().type(torch.int64)
+            print("Box=", box)
+            b0 = box[0].item()
+            print("b-0=", b0)
+            cv2.rectangle(cv_image, (box[0].item(), box[1].item()), (box[2].item(), box[3].item()), (255, 255, 0), 4)
+            det = Detection2D()
+            det.header = msg.header
+            object_hypothesis_with_pose = ObjectHypothesisWithPose()
+            object_hypothesis = ObjectHypothesis()
+            object_hypothesis.class_id = class_labels[label_id]
+            object_hypothesis.score = score.detach().item()
+            object_hypothesis_with_pose.hypothesis = object_hypothesis
+            det.results.append(object_hypothesis_with_pose)
+            bounding_box = BoundingBox2D()
+            bounding_box.center.position.x = float((box[0] + box[2])/2)
+            bounding_box.center.position.y = float((box[1] + box[3])/2)
+            bounding_box.center.theta = 0.0
+            bounding_box.size_x = float(2*(bounding_box.center.position.x - box[0]))
+            bounding_box.size_y = float(2*(bounding_box.center.position.y - box[1]))
+            det.bbox = bounding_box
+            det_array.detections.append(det)
+        self.publisher.publish(det_array)
+        self.render_image_publisher.publish(self.bridge.cv2_to_imgmsg(cv_image))
 
 
 ap = argparse.ArgumentParser(description="MobileNet Object Detector")
