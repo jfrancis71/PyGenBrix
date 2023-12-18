6298165aa7cc196b0f5de1f82880699ef07aeb59
Julian <julian.w.francis@gmail.com>
2023-12-18 16:36:44 +0000

    Moving to git repository thomas

--- thomas/robot/camera.sh
+++
@@ -1,3 +0,0 @@
-# Must be run in conda RobotStack environment.
-ros2 run image_tools cam2image &
-ros2 run image_transport republish raw compressed --ros-args --remap in:=/image --remap out/compressed:=thomas/compressed &
